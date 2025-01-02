54d30f98182303ae89f40bcc149945630de2ebcc
Barys <barys.radzko@protonmail.com>
2025-01-02 15:58:21 +0100

    removed documentation cargo doc --open saves it locally

--- Projects/guessing_game/target/debug/deps/byteorder-db27a0ce4506c654.d
+++
@@ -1,5 +0,0 @@
-/home/bk/Documents/Rust/Projects/guessing_game/target/debug/deps/libbyteorder-db27a0ce4506c654.rmeta: /home/bk/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/home/bk/Documents/Rust/Projects/guessing_game/target/debug/deps/byteorder-db27a0ce4506c654.d: /home/bk/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/home/bk/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs:
