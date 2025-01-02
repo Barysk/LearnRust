a71132b4a9f7ecfe174532730fb31289e0c061af
Barys <barys.radzko@protonmail.com>
2025-01-02 13:32:54 +0100

    Cargos were examined, no bomb inside

---
+++ Projects/hello_cargo/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// 'cargo init' if you want to convert an existing rust project to cargo one
+// Cargo Structure
+// |-Cargo.toml
+// |-src
+// | |- source code
+// |
+// |-Anything like Readme, or License
+//
+// Easier to just use 'cargo new <project name>' to create one
+// 'cargo build' for building a project // Debug by default
+// 'cargo run' for runnning it
+// 'cargo check' to check if project compiles
+//
+// 'cargo build --release' to use release optimisations
---
+++ Projects/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/bk/Documents/Rust/Projects/hello_cargo/target/release/hello_cargo: /home/bk/Documents/Rust/Projects/hello_cargo/src/main.rs
