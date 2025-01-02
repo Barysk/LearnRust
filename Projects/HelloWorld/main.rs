6e251a65eca29e2f5eeafbf7c928925e4782a38a
Barys <barys.radzko@protonmail.com>
2025-01-02 13:09:49 +0100

    My Rust has Hello Worlded

---
+++ Projects/HelloWorld/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!"); // ! is needed to call a macro, not a function
+                               // What is macros will be discussed later
+                               // without the ! it would be a function call
+}
+
+// there is a tool 'rustfmt' that formats all file automaticly
+// 'rustc' to compile - fair enough
